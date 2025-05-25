31c6d09307fa0220526627589a3102fe0c2ebc10
Mccucumber <ufukberkbayraktar@gmail.com>
2025-05-25 22:44:17 +0300

    a

--- iletişim/contact.php
+++ iletişim/contact.php
@@ -1,8 +1,19 @@
 <?php
-    $name = $_POST[''];
-    $visitor_email = $_POST[''];
-    $subject = $_POST[''];
-    $massage = $_POST[''];
-     a
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $text = $_POST['text'];
+    $massage = $_POST['mesaj'];
     
+    $email_from = 'İletişim@iletişim.com';
+    $email_konu = 'Yeni İletişim Formu';
+    $email_body = "Gönderen Adı: $name.\n".    
+                    "Gönderen Maili: $visitor_email.\n".
+                    "Konu: $text.\n".
+                    "Gönderen Mesajı: $message.\n";
+    
+    $to = 'ufukberkbayraktar@gmail.com';
+    $headers = "From: $email_from\r\n";
+    $headers .="Cevap-Ver $visitor_email\r\n";
+    mail($to,$email_konu,$email_body,$headers);
+    header("Location: contact.html");
 ?>
